2a722c7b3cc76516d3bacb29a42e2d3f34287bb3
Charlie Eysbroek <C.eysbroek@hotmail.com>
2023-02-12 10:25:50 +0100

    fix on main.js while low ram

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
 /*Creator: Charles, add me on github https://github.com/MrPeanutbutterz 
 Proces: this script controls all scripts based on what is needed for next goal */
 
-import { getScriptsPath } from "./Default/config.js"
+import { getSleepTime, getScriptsPath } from "./Default/config.js"
 
 /** @param {NS} ns */
 export async function main(ns) {
 
 	//\\ SCRIPT SETTINGS
 	ns.disableLog("ALL")
-	ns.clearLog()
 
 	//\\ GENERAL DATA
 	let script = getScriptsPath(ns)
+	let speed = getSleepTime(ns)
 
 	//\\ SCRIPT SPECIFIC FUNCTIONS
+	let scripts = [
+		script.buyRam,
+		script.buyHacknet,
+		script.buyPrograms,
+		script.netStumbler,
+		script.buyServers,
+		script.findFaction,
+		script.metaSploit,
+		script.buyCore
+	]
+
 	//\\ MAIN LOGICA
-	ns.run(script.buffer, 1, script.buyRam, script.buyHacknet, script.buyPrograms, script.netStumbler, script.buyServers, script.findFaction, script.metaSploit, script.buyCore)
+	for (let i = 0; i < scripts.length;) {
+
+		if ((ns.getServerMaxRam("home") - ns.getServerUsedRam("home")) > ns.getScriptRam(scripts[i])) {
+			ns.run(scripts[i], 1)
+			i++
+		} else {
+			ns.clearLog()
+			ns.print("awaiting ram for " + scripts[i])
+			await ns.sleep(speed.slow)
+		}
+	}
 }
