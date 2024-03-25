0bbaaafaefa5e676d975c3e3b95a4122d1836a74
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-03-25 12:00:11 +0100

    formatting

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*Creator: Charles, add me on github https://github.com/MrPeanutbutterz 
 Proces: this script controls all scripts based on what is needed for next goal */
 
-//Hello World
+//Hello World, its me.
 
 import { getSleepTime, getScriptsPath } from "./Default/config.js"
 
