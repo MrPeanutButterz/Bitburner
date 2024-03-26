46c34df6c69342d242eabcafb2479eb1f853a63e
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-03-26 12:36:18 +0100

    working netscript definitions

---
+++ system.js
@@ -0,0 +1,21 @@
+import { NS } from "@ns";
+
+/** @param {NS} ns */
+export async function main(ns) {
+
+  //\\ SCRIPT SETTINGS
+  ns.disableLog("ALL")
+  ns.clearLog()
+
+  //\\ GENERAL DATA
+  //\\ SCRIPT SPECIFIC FUNCTIONS
+  //\\ MAIN LOGIC
+  ns.tprint("Booting")
+  await ns.sleep(1000)
+  ns.tprint("Analyzing...")
+  await ns.sleep(1000)
+  ns.tprint("INIT Process")
+
+  
+
+}
