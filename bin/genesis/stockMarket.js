9c98714e49972cbeb7ec8383d89d7d026be5b8a8
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-04-05 20:00:10 +0200

    working stockmarket script

--- bin/genesis/stockMarket.js
+++ bin/genesis/stockMarket.js
@@ -1,19 +1,12 @@
-class stock {
-    constructor(ticker) {
-
-        this.ticker = ticker
-
-    }
-}
-
 /** @param {NS} ns */
 export async function main(ns) {
 
     // check all accounts, buy if no present
     // make list of active stocks
-    // add or remove stocks if needed
     // buy en sell stocks
-    // pump n dump
+
+    // buy max shares 
+    // sell max shares
 
     //\\ SCRIPT SETTINGS
     ns.tprint("Active")
@@ -21,92 +14,98 @@ export async function main(ns) {
     ns.clearLog()
 
     //\\ GENERAL DATA
-    let accounts = false
-    let symbols = STOCK.getSymbols()
-    let constants = STOCK.getConstants()
-
-    const STOCK = ns.STOCK
-    const FORCAST_BUY_THRESHOLD = 0.6
+    const FORCAST_BUY_THRESHOLD = 0.65
     const FORCAST_SELL_THRESHOLD = 0.4
     const BALANCE_THRESHOLD = 100_000_000
-    
-    let INTRESTING_STOCKS = []
-    let PORTFOLIO = []
 
     //\\ FUNCTIONS
-    function acquireAccounts() {
-
-        // Checks if all accounts are acquired or buys 
-
-        let myBalance = ns.getServerMoneyAvailable("home")
-
-        if (!STOCK.has4SData()) {
-            if (myBalance > constants.MarketData4SCost) {
-                STOCK.purchase4SMarketData()
-                ns.tprint("4S MarktData bought $" + constants.MarketData4SCost)
-            }
-        } else if (!STOCK.has4SDataTIXAPI()) {
-            if (myBalance > constants.MarketDataTixApi4SCost) {
-                STOCK.purchase4SMarketData()
-                ns.tprint("4S MarktData TIX bought $" + constants.MarketDataTixApi4SCost)
-            }
-        } else if (!STOCK.hasTIXAPIAccess()) {
-            if (myBalance > constants.TIXAPICost) {
-                STOCK.purchase4SMarketData()
-                ns.tprint("TIX API bought $" + constants.TIXAPICost)
-            }
-        } else if (!STOCK.hasWSEAccount()) {
-            if (myBalance > constants.WSEAccountCost) {
-                STOCK.purchase4SMarketData()
-                ns.tprint("WSE Account bought $" + constants.WSEAccountCost)
-            }
-        } else {
-            accounts = true
-
-        }
-    }
-
-    function addPortfolio(sym) {
-        PORTFOLIO.push(sym)
-    }
-
     //\\ MAIN LOGICA
     ns.print("Accounts check...")
-    await ns.sleep(1000)
-    
-    while (!accounts) {
-        acquireAccounts()
-        await ns.sleep(2000)
-        ns.clearLog()
-    }
+    await ns.sleep(2000)
 
-    ns.print("Accounts present")
-    await ns.sleep(1000)
-    ns.print("Managing Stocks")
-
-    while (true) {
+    while (!ns.stock.hasWSEAccount()
+        || !ns.stock.has4SData()
+        || !ns.stock.hasTIXAPIAccess()
+        || !ns.stock.has4SDataTIXAPI()) {
 
         await ns.sleep(1000)
         ns.clearLog()
 
-        symbols.forEach(sym => {
+        // buy Wse
+        if (!ns.stock.hasWSEAccount()) {
+            if (ns.stock.purchaseWseAccount()) { ns.print("WSE account found...") } else { ns.print("No WSE account") }
+        }
 
-            if (STOCK.getForecast(sym) >= FORCAST_BUY_THRESHOLD) {
+        // buy 4S data
+        if (!ns.stock.has4SData()) {
+            if (ns.stock.purchase4SMarketData()) { ns.print("4S data account found...") } else { ns.print("No 4S data account") }
+        }
 
-                // buy STOCK
-                ns.print(
-                    sym +
-                    " forcast:" + Math.floor(STOCK.getForecast(sym) * 100) +
-                    " volatility:" + (STOCK.getVolatility(sym) * 100).toPrecision(3)
-                )
+        // buy Tix api
+        if (!ns.stock.has4SDataTIXAPI()) {
+            if (ns.stock.purchase4SMarketDataTixApi()) { ns.print("Tix Api account found...") } else { ns.print("No Tix Api account") }
+        }
 
+        // buy 4s Tix api access
+        if (!ns.stock.hasTIXAPIAccess()) {
+            if (ns.stock.purchaseTixApi()) { ns.print("Tix Api access account found...") } else { ns.print("No Tix Api access account") }
+        }
+    }
 
-            } else if (STOCK.getForecast(sym) < FORCAST_SELL_THRESHOLD) {
+    ns.print("Accounts are present")
+    await ns.sleep(2000)
 
-                // sell stock
+    ns.print("Managing Stocks")
+
+    const symbols = ns.stock.getSymbols()
+
+    while (true) {
+
+        await ns.stock.nextUpdate()
+        ns.clearLog()
+
+        for (let sym of symbols) {
+
+            ns.print(sym + "\t Forcast " + ns.stock.getForecast(sym).toPrecision(3))
+
+            if (ns.stock.getPosition(sym)[0] > 0) {
+
+                ns.print("INFO Shares")
+                ns.print("INFO " + ns.stock.getPosition(sym)[0] + " / " + ns.stock.getMaxShares(sym))
+                ns.print(" ")
+
 
             }
-        })
+
+            if (ns.stock.getForecast(sym) > FORCAST_BUY_THRESHOLD && ns.stock.getPosition(sym)[0] === 0) {
+
+                let maxShares = ns.stock.getMaxShares(sym)
+                let askPrice = ns.stock.getAskPrice(sym)
+                let purchaseCost = Math.ceil(ns.stock.getPurchaseCost(sym, ns.stock.getMaxShares(sym), "Long"))
+                
+                ns.print("WARN BUY")
+                
+                if (ns.getServerMoneyAvailable("home") > purchaseCost) {
+                    ns.stock.buyStock(sym, maxShares)
+                    ns.print("Shares: " + ns.stock.getPosition(sym)[0])
+                    
+                } else {
+                    ns.print("PurchaseCost: " + purchaseCost)
+                    ns.print("We don't have the money...")
+                }
+                ns.print(" ")
+
+            }
+
+            if (ns.stock.getForecast(sym) < FORCAST_SELL_THRESHOLD && ns.stock.getPosition(sym)[0] > 0) {
+
+                ns.print("WARN SELL")
+                ns.print(" ")
+
+                ns.stock.sellStock(sym, ns.stock.getPosition(sym)[0])
+
+            }
+        }
     }
 }
 
@@ -124,8 +123,8 @@ export async function main(ns) {
 // getAskPrice(sym)	                            Returns the ask price of that stock.
 // getBidPrice(sym)	                            Returns the bid price of that stock.
 // cancelOrder(sym, shares, price, type, pos)	Cancel order for stocks.
-// buyStock(sym, shares)	                    Buy stocks.
 // getSaleGain(sym, shares, posType)	        Calculate profit of selling stocks.
+// buyStock(sym, shares)	                    Buy stocks.
 // sellStock(sym, shares)	                    Sell stocks.
 
 // getOrganization(sym)	                        Returns the organization associated with a stock symbol.
@@ -133,8 +132,3 @@ export async function main(ns) {
 // placeOrder(sym, shares, price, type, pos)	Place order for stocks.
 // buyShort(sym, shares)                        Short stocks.
 // sellShort(sym, shares)	                    Sell short stock.
-
-
-// Rename scripts
-// networkStage1.js
-// stockStage1.js
