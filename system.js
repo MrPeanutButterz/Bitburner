2f5ac9a5566eb311dd681695d79372f7ff1c7869
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-04-09 00:14:24 +0200

    rework boot

--- system.js
+++ system.js
@@ -6,18 +6,6 @@ export async function main(ns) {
   ns.clearLog()
 
   //\\ GENERAL DATA
-  const home = "home"
-
-  const scripts = [
-    "bin/genesis/collectStage1.js", // 0
-    "purchase/hacknet.js",          // 1
-    "purchase/servers.js",          // 2
-    "purchase/ram.js",              // 3
-    "purchase/programs.js",         // 4
-    "bin/genesis/stockMarket.js",   // 5
-    "bin/genesis/collectStage3.js", // 6
-  ]
-
   //\\ FUNCTIONS 
   function calculateHomeRam(script) {
     let ramAvailable = ns.getServerMaxRam("home") - ns.getServerUsedRam("home")
@@ -30,40 +18,46 @@ export async function main(ns) {
   await ns.sleep(2000)
 
   // collect
-  while (!calculateHomeRam(scripts[0])) { await ns.sleep(1000) }
-  ns.run(scripts[0], 1, "foodnstuff")
+  while (!calculateHomeRam("bin/genesis/collectStage1.js")) { await ns.sleep(1000) }
+  ns.run("bin/genesis/collectStage1.js", 1, "foodnstuff")
   await ns.sleep(1000)
 
   // // hacknet
-  // while (!calculateHomeRam(scripts[1])) { await ns.sleep(1000) }
-  // ns.run(scripts[1], 1, 10, 100, 4, 1)
+  // while (!calculateHomeRam("purchase/hacknet.js")) { await ns.sleep(1000) }
+  // ns.run("purchase/hacknet.js", 1, 10, 100, 4, 1)
   // await ns.sleep(1000)
 
   // servers limited 32Gb
-  while (!calculateHomeRam(scripts[2])) { await ns.sleep(1000) }
-  ns.run(scripts[2], 1, 32)
+  while (!calculateHomeRam("purchase/servers.js")) { await ns.sleep(1000) }
+  ns.run("purchase/servers.js", 1, 32)
   await ns.sleep(1000)
 
   // ram
-  while (!calculateHomeRam(scripts[3])) { await ns.sleep(1000) }
-  ns.run(scripts[3], 1)
+  while (!calculateHomeRam("purchase/ram.js")) { await ns.sleep(1000) }
+  ns.run("purchase/ram.js", 1)
   await ns.sleep(1000)
 
   // programs
-  while (!calculateHomeRam(scripts[4])) { await ns.sleep(1000) }
-  ns.run(scripts[4], 1)
+  while (!calculateHomeRam("purchase/programs.js")) { await ns.sleep(1000) }
+  ns.run("purchase/programs.js", 1)
   await ns.sleep(1000)
 
   // servers 
-  while (!calculateHomeRam(scripts[2]) || ns.isRunning(scripts[2], "home", 32)) { await ns.sleep(1000) }
-  ns.run(scripts[2], 1)
+  while (!calculateHomeRam("purchase/servers.js") || ns.isRunning("purchase/servers.js", "home", 32)) { await ns.sleep(1000) }
+  ns.run("purchase/servers.js", 1)
   await ns.sleep(1000)
 
-  // stockmarket
   if (ns.getServerMaxRam("home") > 1024) {
-    while (!calculateHomeRam(scripts[4]) && ns.isRunning(scripts[6], "home")) { await ns.sleep(1000) }
-    ns.run(scripts[5], 1)
+    
+    // stockmarket
+    while (!calculateHomeRam("bin/genesis/stockMarket.js") && ns.isRunning("bin/genesis/collectStage3.js", "home")) { await ns.sleep(1000) }
+    ns.run("bin/genesis/stockMarket.js", 1)
     await ns.sleep(1000)
-  }
+    
+    // core
+    while (!calculateHomeRam("purchase/core.js")) { await ns.sleep(1000) }
+    ns.run("purchase/core.js", 1)
+    await ns.sleep(1000)
 
+  }
 }
