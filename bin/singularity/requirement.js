4203fa4b2a2523adce821dc34f68e23ac1df9c0f
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-04-14 01:09:03 +0200

    tweeks

--- bin/singularity/requirement.js
+++ bin/singularity/requirement.js
@@ -59,6 +59,17 @@ export async function main(ns) {
         }
     }
 
+    function runTheCompany(corp, rep) {
+        if (!ns.scriptRunning(SCRIPT.company, "home")) {
+            let ramAvailable = ns.getServerMaxRam("home") - ns.getServerUsedRam("home")
+            if (ramAvailable > ns.getScriptRam(SCRIPT.company, "home")) {
+
+
+                ns.run(SCRIPT.company, 1, corp, rep)
+            }
+        }
+    }
+
     //\\ MAIN LOGIC
     while (true) {
 
@@ -86,10 +97,10 @@ export async function main(ns) {
             FACTION === "The Black Hand" ||
             FACTION === "BitRunners") {
 
-            // "CyberSec" 					    //Install a backdoor on the CSEC server
-            // "NiteSec"						//Install a backdoor on the avmnite-02h server
-            // "The Black Hand"				    //Install a backdoor on the I.I.I.I server
-            // "BitRunners"					    //Install a backdoor on the run4theh111z server
+            // "CyberSec" 					    // Install a backdoor on the CSEC server
+            // "NiteSec"						// Install a backdoor on the avmnite-02h server
+            // "The Black Hand"				    // Install a backdoor on the I.I.I.I server
+            // "BitRunners"					    // Install a backdoor on the run4theh111z server
 
             await installBackdoor(ns, SERVER)
 
@@ -102,13 +113,13 @@ export async function main(ns) {
             FACTION === "Aevum" ||
             FACTION === "Volhaven") {
 
-            // "Tian Di Hui"					//$1m & Hacking lvl 50 & Be in Chongqing, New Tokyo, or Ishima
-            // "Sector-12"					    //Be in Sector-12 & $15m
-            // "Chongqing"					    //Be in Chongqing & $20m
-            // "New Tokyo"					    //Be in New Tokyo & $20m
-            // "Ishima"						    //Be in Ishima & $30m
-            // "Aevum"						    //Be in Aevum & $40m
-            // "Volhaven"						//Be in Volhaven & $50m
+            // "Tian Di Hui"					// $1m & Hacking lvl 50 & Be in Chongqing, New Tokyo, or Ishima
+            // "Sector-12"					    // Be in Sector-12 & $15m
+            // "Chongqing"					    // Be in Chongqing & $20m
+            // "New Tokyo"					    // Be in New Tokyo & $20m
+            // "Ishima"						    // Be in Ishima & $30m
+            // "Aevum"						    // Be in Aevum & $40m
+            // "Volhaven"						// Be in Volhaven & $50m
 
             if (ns.getServerMoneyAvailable("home") < FACTION_STATS.money) {
 
@@ -137,29 +148,27 @@ export async function main(ns) {
             FACTION === "Clarke Incorporated" ||
             FACTION === "Fulcrum Secret Technologies") {
 
-            // "ECorp"						    //Have 400K reputation, Backdooring company server reduces faction requirement to 300k
-            // "MegaCorp"						//Have 400K reputation, Backdooring company server reduces faction requirement to 300k
-            // "KuaiGong International"		    //Have 400K reputation, Backdooring company server reduces faction requirement to 300k
-            // "Four Sigma"					    //Have 400K reputation, Backdooring company server reduces faction requirement to 300k
-            // "NWO"							//Have 400K reputation, Backdooring company server reduces faction requirement to 300k
-            // "Blade Industries"				//Have 400K reputation, Backdooring company server reduces faction requirement to 300k
-            // "OmniTek Incorporated"			//Have 400K reputation, Backdooring company server reduces faction requirement to 300k
-            // "Bachman & Associates"			//Have 400K reputation, Backdooring company server reduces faction requirement to 300k
-            // "Clarke Incorporated"			//Have 400K reputation, Backdooring company server reduces faction requirement to 300k
-            // "Fulcrum Secret Technologies" 	//Have 500K reputation, Backdooring company server reduces faction requirement to 400K
+            // "ECorp"						    // Have 400K reputation, Backdooring company server reduces faction requirement to 300k
+            // "MegaCorp"						// Have 400K reputation, Backdooring company server reduces faction requirement to 300k
+            // "KuaiGong International"		    // Have 400K reputation, Backdooring company server reduces faction requirement to 300k
+            // "Four Sigma"					    // Have 400K reputation, Backdooring company server reduces faction requirement to 300k
+            // "NWO"							// Have 400K reputation, Backdooring company server reduces faction requirement to 300k
+            // "Blade Industries"				// Have 400K reputation, Backdooring company server reduces faction requirement to 300k
+            // "OmniTek Incorporated"			// Have 400K reputation, Backdooring company server reduces faction requirement to 300k
+            // "Bachman & Associates"			// Have 400K reputation, Backdooring company server reduces faction requirement to 300k
+            // "Clarke Incorporated"			// Have 400K reputation, Backdooring company server reduces faction requirement to 300k
+            // "Fulcrum Secret Technologies" 	// Have 500K reputation, Backdooring company server reduces faction requirement to 400K
 
             await installBackdoor(ns, SERVER)
 
             displayLog("Runnig the company")
-            if (!ns.scriptRunning(SCRIPT.company, "home")) {
+            if (FACTION === "Fulcrum Secret Technologies") {
 
-                let ramAvailable = ns.getServerMaxRam("home") - ns.getServerUsedRam("home")
-                if (ramAvailable > ns.getScriptRam(SCRIPT.company, "home")) {
+                runTheCompany("Fulcrum Technologies", 4e5)
 
-                    FACTION === "Fulcrum Secret Technologies" ?
-                        ns.run(SCRIPT.company, 1, "Fulcrum Technologies", 4e5) :
-                        ns.run(SCRIPT.company, 1, FACTION, 3e5)
-                }
+            } else {
+
+                runTheCompany(FACTION, 3e5)
             }
 
         } else if (
@@ -170,12 +179,12 @@ export async function main(ns) {
             FACTION === "The Dark Army" ||
             FACTION === "The Syndicate") {
 
-            // "Slum Snakes"					//All Combat Stats of 30, -9 Karma, $1m
-            // "Tetrads"						//Be in Chongqing, New Tokyo, or Ishima, All Combat Stats of 75, -18 Karma
-            // "Silhouette"					    //CTO, CFO, or CEO of a company, $15m, -22 Karma
-            // "Speakers for the Dead"		    //Hacking lvl 100, All Combat Stats of 300, 30 People Killed, -45 Karma, Not working for CIA or NSA
-            // "The Dark Army"				    //Hacking lvl 300, All Combat Stats of 300, Be in Chongqing, 5 People Killed, -45 Karma, Not working for CIA or NSA
-            // "The Syndicate"				    ///Hacking lvl 200, All Combat Stats of 200, Be in Aevum or Sector-12, $10m, -90 Karma, Not working for CIA or NSA
+            // "Slum Snakes"					// All Combat Stats of 30, -9 Karma, $1m
+            // "Tetrads"						// Be in Chongqing, New Tokyo, or Ishima, All Combat Stats of 75, -18 Karma
+            // "Silhouette"					    // CTO, CFO, or CEO of a company, $15m, -22 Karma
+            // "Speakers for the Dead"		    // Hacking lvl 100, All Combat Stats of 300, 30 People Killed, -45 Karma, Not working for CIA or NSA
+            // "The Dark Army"				    // Hacking lvl 300, All Combat Stats of 300, Be in Chongqing, 5 People Killed, -45 Karma, Not working for CIA or NSA
+            // "The Syndicate"				    // Hacking lvl 200, All Combat Stats of 200, Be in Aevum or Sector-12, $10m, -90 Karma, Not working for CIA or NSA
 
             if (ns.getPlayer().skills.strength < FACTION_STATS.strength ||
                 ns.getPlayer().skills.defense < FACTION_STATS.defense ||
@@ -184,10 +193,10 @@ export async function main(ns) {
 
                 displayLog("Pumping at the gym brb...")
                 goToGym(FACTION_STATS.strength, FACTION_STATS.defense, FACTION_STATS.dexterity, FACTION_STATS.agility)
-                
+
             } else if (ns.getPlayer().numPeopleKilled < FACTION_STATS.skills ||
-            ns.getPlayer().karma > FACTION_STATS.karma) {
-                
+                ns.getPlayer().karma > FACTION_STATS.karma) {
+
                 displayLog("Doing some crime...")
                 doSomeCrime(FACTION_STATS.kills, FACTION_STATS.karma)
 
@@ -216,9 +225,9 @@ export async function main(ns) {
             FACTION === "Daedalus" ||
             FACTION === "Illuminati") {
 
-            // "The Covenant"					//20 Augmentations, $75b, Hacking lvl of 850, All Combat Stats of 850
-            // "Daedalus"						//30 Augmentations, $100b, Hacking lvl of 2500 OR All Combat Stats of 1500
-            // "Illuminati"					    //30 Augmentations, $150b, Hacking lvl of 1500, All Combat Stats of 1200
+            // "The Covenant"					// 20 Augmentations, $75b, Hacking lvl of 850, All Combat Stats of 850
+            // "Daedalus"						// 30 Augmentations, $100b, Hacking lvl of 2500 OR All Combat Stats of 1500
+            // "Illuminati"					    // 30 Augmentations, $150b, Hacking lvl of 1500, All Combat Stats of 1200
 
             if (ns.getPlayer().skills.strength < FACTION_STATS.strength ||
                 ns.getPlayer().skills.defense < FACTION_STATS.defense ||
