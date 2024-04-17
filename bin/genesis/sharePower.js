fd78ba6df24fbd8e5813a11b6c4b6070bfb43a41
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-04-17 13:59:06 +0200

    added flag commands

--- bin/genesis/sharePower.js
+++ bin/genesis/sharePower.js
@@ -1,41 +1,65 @@
 import { scriptStart, scriptExit, scriptPath } from "lib/scripting"
-import { NmapClear, NmapRamServers, NmapTotalRam } from "lib/network"
+import { NmapClear, NmapRamServers } from "lib/network"
 
 /** @param {NS} ns */
 export async function main(ns) {
 
     //\\ SCRIPT SETTINGS
+    const flags = ns.flags([
+        ["home", false],
+        ["network", false]
+    ])
     scriptStart(ns)
 
     //\\ GENERAL DATA
     const SCRIPT = scriptPath(ns)
 
-    //\\ MAIN LOGICA
-    if (ns.scriptRunning(SCRIPT.collectStage2, "home")) { ns.scriptKill(SCRIPT.collectStage2, "home") }
-    if (ns.scriptRunning(SCRIPT.preweak, "home")) { ns.scriptKill(SCRIPT.preweak, "home") }
-
-    NmapClear(ns)
-    while (NmapTotalRam(ns) != 26216588) {
-
-        await ns.sleep(1000)
-        NmapRamServers(ns).forEach(server => {
-
-            if (ns.hasRootAccess(server)) {
-                if (ns.scp(SCRIPT.share, server, "home")) {
-
-                    let ramAvailable = ns.getServerMaxRam(server) - ns.getServerUsedRam(server)
-                    let threadsAvailable = Math.floor(ramAvailable / ns.getScriptRam(SCRIPT.share))
-                    if (threadsAvailable > 1) {
-                        ns.exec(SCRIPT.share, server, threadsAvailable)
-                    }
-
-                }
-            }
-        })
+    //\\ FUNCTIONS
+    function shareHome() {
+        let availableRam = ns.getServerMaxRam("home") * 0.9 - ns.getServerUsedRam("home")
+        let availableThreads = Math.floor(availableRam / ns.getScriptRam(SCRIPT.share))
+        if (availableThreads > 1) { ns.exec(SCRIPT.share, "home", availableThreads) }
     }
 
-    let availableRam = ns.getServerMaxRam("home") - ns.getServerUsedRam("home")
-    let availableThreads = Math.floor(availableRam / ns.getScriptRam(SCRIPT.share))
-    if (availableThreads > 1) { ns.exec(SCRIPT.share, "home", availableThreads) }
+    async function shareNetwork() {
+
+        if (ns.scriptRunning(SCRIPT.collectStage2, "home")) { ns.scriptKill(SCRIPT.collectStage2, "home") }
+        if (ns.scriptRunning(SCRIPT.preweak, "home")) { ns.scriptKill(SCRIPT.preweak, "home") }
+
+        NmapClear(ns)
+        while (true) {
+
+            await ns.sleep(1000)
+            NmapRamServers(ns).forEach(server => {
+
+                if (ns.hasRootAccess(server)) {
+                    if (ns.scp(SCRIPT.share, server, "home")) {
+
+                        let ramAvailable = ns.getServerMaxRam(server) - ns.getServerUsedRam(server)
+                        let threadsAvailable = Math.floor(ramAvailable / ns.getScriptRam(SCRIPT.share))
+                        if (threadsAvailable > 1) {
+                            ns.exec(SCRIPT.share, server, threadsAvailable)
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+    //\\ MAIN LOGICA
+    if (flags.home) {
+
+        shareHome()
+
+    } else if (flags.network) {
+
+        shareNetwork()
+
+    } else {
+
+        shareNetwork()
+        shareHome()
+
+    }
     scriptExit(ns)
 } 
