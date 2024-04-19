fba7cf5d6c0f75d4ce6b32e9087f51a97831fd93
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-04-19 17:56:22 +0200

    custom UI

---
+++ utils/customUI.js
@@ -0,0 +1,45 @@
+import { NmapTotalRam } from "lib/network"
+
+/** @param {NS} ns */
+export async function main(ns) {
+
+    //\\ SCRIPT SETTINGS
+    ns.disableLog("ALL")
+    ns.clearLog()
+
+    //\\ GENERAL DATA
+    const HOOK0 = document.getElementById("overview-extra-hook-0")
+    const HOOK1 = document.getElementById("overview-extra-hook-1")
+
+    //\\ FUNCTIONS 
+    function UIlog() {
+
+        const slots = [
+            ["Income", ns.formatNumber(ns.getTotalScriptIncome()[1])],
+            ["NetRam", ns.formatRam(NmapTotalRam(ns))],
+        ]
+
+        let source = [], value = []
+        slots.forEach(slot => { source.push(slot[0]); value.push(slot[1]) })
+
+        try {
+
+            HOOK0.innerText = source.join("\n")
+            HOOK1.innerText = value.join("\n")
+
+        } catch (e) { ns.tprint(e) }
+        ns.atExit(() => UIclear())
+    }
+
+    function UIclear() {
+        HOOK0.innerText = ""
+        HOOK1.innerText = ""
+    }
+
+    //\\ LOGIC
+    while (true) {
+        await ns.sleep(1000)
+        ns.clearLog()
+        UIlog()
+    }
+}
