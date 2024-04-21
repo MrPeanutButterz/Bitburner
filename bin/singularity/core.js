83ed91d9f90a5427ead32caff52ba694b5263ae6
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-04-21 13:24:22 +0200

    print msg to terminal

--- bin/singularity/core.js
+++ bin/singularity/core.js
@@ -5,23 +5,13 @@ export async function main(ns) {
 
     //\\ SCRIPT SETTINGS
     scriptStart(ns)
-    
-    //\\ GENERAL DATA
+
     //\\ MAIN LOGICA
     while (true) {
         await ns.sleep(1000)
-        ns.clearLog()
-
-		if (ns.getPlayer().money > ns.singularity.getUpgradeHomeCoresCost()) {
-
-			ns.singularity.upgradeHomeCores()
-			ns.toast("Core upgrade", "success", 6000)
-
-		} else {
-
-            ns.print("Next core upgrade cost $" + Math.ceil(ns.singularity.getUpgradeHomeCoresCost()))
-            ns.print(Math.round(ns.getPlayer().money / ns.singularity.getUpgradeHomeCoresCost() * 100) + "% of money is avaliable")
-			
-		}
+        if (ns.getPlayer().money > ns.singularity.getUpgradeHomeCoresCost() &&
+            ns.singularity.upgradeHomeCores()) {
+            ns.tprint("Core upgraded")
+        }
     }
 }
--- bin/singularity/ram.js
+++ bin/singularity/ram.js
@@ -6,22 +6,12 @@ export async function main(ns) {
     //\\ SCRIPT SETTINGS
     scriptStart(ns)
 
-    //\\ GENERAL DATA   
     //\\ MAIN LOGICA
     while (true) {
         await ns.sleep(1000)
-        ns.clearLog()
-
-        if (ns.getPlayer().money > ns.singularity.getUpgradeHomeRamCost()) {
-
-            ns.singularity.upgradeHomeRam()
-            ns.toast("Ram upgrade", "success", 6000)
-
-        } else {
-
-            ns.print("Next ram upgrade cost $" + Math.ceil(ns.singularity.getUpgradeHomeRamCost()))
-            ns.print(Math.round(ns.getPlayer().money / ns.singularity.getUpgradeHomeRamCost() * 100) + "% of money is avaliable")
-
+        if (ns.getPlayer().money > ns.singularity.getUpgradeHomeRamCost() &&
+            ns.singularity.upgradeHomeRam()) {
+            ns.tprint("Ram upgraded")
         }
     }
 }
