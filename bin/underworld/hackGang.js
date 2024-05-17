a893de12da8c2e22f83fc9ab61549dca63b6ab2d
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-05-17 11:49:25 +0200

    small tweeks

--- bin/underworld/hackGang.js
+++ bin/underworld/hackGang.js
@@ -23,8 +23,19 @@ export async function main(ns) {
     const WANTED_LOWERBAND = 1
     const WANTED_UPPERBAND = 10
 
-    const AUGMENTATIONS = ["BitWire", "Neuralstimulator", "DataJack"]
-    const ROOTKITS = ["NUKE Rootkit", "Soulstealer Rootkit", "Demon Rootkit", "Hmap Node", "Jack the Ripper"]
+    const AUGMENTATIONS = [
+        "BitWire",
+        "Neuralstimulator",
+        "DataJack"
+    ]
+
+    const ROOTKITS = [
+        "NUKE Rootkit",
+        "Soulstealer Rootkit",
+        "Demon Rootkit",
+        "Hmap Node",
+        "Jack the Ripper"
+    ]
 
     let memberNum = 0
 
@@ -46,9 +57,10 @@ export async function main(ns) {
         let list = []
         for (let member of API.getMemberNames()) {
             if (API.getInstallResult(member)) {
-                list.push({ name: member, ascend: (API.getAscensionResult(member).hack * API.getInstallResult(member).hack).toFixed(4) })
+                list.push({ name: member, ascend: (API.getAscensionResult(member).hack * API.getInstallResult(member).hack).toFixed(5) })
             }
         }
+
         list.sort((a, b) => a.ascend - b.ascend).reverse()
         ns.print("\nMembers ascend @" + ASCEND_THRESHOLD)
         list.forEach(i => { ns.print(i.name + "\t" + i.ascend) })
@@ -75,11 +87,14 @@ export async function main(ns) {
 
         // require a member en set to work asap
 
-        if (API.canRecruitMember()) {
-            for (var i = 0; i < API.getRecruitsAvailable(); i++) {
-                let name = createMemberName()
-                API.recruitMember(name)
-                API.setMemberTask(name, TASK_WANTED)
+        if (API.getMemberNames().lenght < 12) {
+
+            if (API.canRecruitMember()) {
+                for (var i = 0; i < API.getRecruitsAvailable(); i++) {
+                    let name = createMemberName()
+                    API.recruitMember(name)
+                    API.setMemberTask(name, TASK_WANTED)
+                }
             }
         }
     }
@@ -184,9 +199,11 @@ export async function main(ns) {
     }
 
     //\\ LOGIC
-    if (API.inGang()) {
+    while (!API.inGang()) {
+
+        await ns.sleep(1000)
         if (API.createGang("NiteSec")) {
-            ns.tprint("Hackgang created")
+            ns.tprint("Hackgang created at NiteSec")
         }
     }
 
@@ -196,8 +213,7 @@ export async function main(ns) {
         display()
         await API.nextUpdate()
 
-        if (API.getBonusTime() > 5000 &&
-            API.getMemberNames().lenght < 11) {
+        if (API.getBonusTime() > 5000) {
 
             recruitMembers()
             memberTaskBonus()
@@ -210,44 +226,3 @@ export async function main(ns) {
         }
     }
 }
-
-// hackging gangs is more straight forward
-// combat gangs for teritory
-
-// nextUpdate()	                                Sleeps until the next Gang update has happened.
-
-// inGang()	                                    Check if you're in a gang.
-// createGang(faction)	                        Create a gang.
-// getGangInformation()	                        Get information about your gang.
-// getBonusTime()	                            Get bonus time.
-
-// canRecruitMember()	                        Check if you can recruit a new gang member.
-// getRecruitsAvailable()	                    Check how many gang members you can currently recruit.
-// recruitMember(name)	                        Recruit a new gang member.
-// renameMember(memberName, newName)	        Rename a Gang member to a new unique name.
-
-// respectForNextRecruit()	                    Check the amount of Respect needed for your next gang recruit.
-// getMemberNames()	                            List all gang members.
-// getMemberInformation(name)	                Get information about a specific gang member.
-// getAscensionResult(memberName)	            Get the result of an ascension without ascending.
-// ascendMember(memberName)	                    Ascend a gang member.
-// getInstallResult(memberName)	                Get the effect of an install on ascension multipliers without installing.
-
-// getTaskNames()	                            List member task names.
-// getTaskStats(name)	                        Get stats of a task.
-// setMemberTask(memberName, taskName)          Set gang member to task.
-
-// getEquipmentNames()	                        List equipment names.
-// getEquipmentStats(equipName)	                Get stats of an equipment.
-// getEquipmentType(equipName)	                Get type of an equipment.
-// getEquipmentCost(equipName)	                Get cost of equipment.
-// purchaseEquipment(memberName, equipName)	    Purchase an equipment for a gang member.
-
-// getOtherGangInformation()	                Get information about the other gangs.
-// setTerritoryWarfare(engage)	                Enable/Disable territory clashes.
-// getChanceToWinClash(gangName)	            Get chance to win clash with other gang.
-
-
-// Certain Factions (Slum Snakes, Tetrads, The Syndicate, The Dark Army, Speakers for the Dead, NiteSec, The Black Hand)
-// give the player the ability to form and manage their own gang, which can earn the player money and reputation with the corresponding Faction.
-// Gangs offer more Augmentations than Factions, and in BitNode-2 offer a way to destroy the BitNode.
