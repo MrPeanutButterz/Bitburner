1c060f4a974a8556eb14292a3198c8c387ab13c3
Charlie Eysbroek <C.eysbroek@hotmail.com>
2024-06-29 11:40:14 +0200

    remove comments

--- bin/underworld/combatGang.js
+++ bin/underworld/combatGang.js
@@ -12,13 +12,10 @@ export async function main(ns) {
      *              |___/             
      * 
      * ✅ create gang
-     * 
      * ✅ management > recruite gang members
      * ✅ management > set task
      * ✅ management > ascend members
-     * 
-     * equipment > buy augmentations
-     * 
+     * ✅ equipment > buy augmentations
      * ✅ territory > set clash
     */
 
@@ -286,44 +283,3 @@ export async function main(ns) {
         }
     }
 }
-
-// hackging gangs is more straight forward
-// combat gangs for teritory
-
-// nextUpdate()	                                Sleeps until the next Gang update has happened.
-
-// inGang()	                                    Check if you're in a gang.
-// createGang(faction)	                        Create a gang.
-// getGangInformation()	                        Get information about your gang.
-// getBonusTime()	                            Get bonus time.
-
-// canRecruitMember()	                        Check if you can recruit a new gang member.
-// getRecruitsAvailable()	                    Check how many gang members you can currently recruit.
-// recruitMember(name)	                        Recruit a new gang member.
-// renameMember(memberName, newName)	        Rename a Gang member to a new unique name.
-
-// respectForNextRecruit()	                    Check the amount of Respect needed for your next gang recruit.
-// getMemberNames()	                            List all gang members.
-// getMemberInformation(name)	                Get information about a specific gang member.
-// getAscensionResult(memberName)	            Get the result of an ascension without ascending.
-// ascendMember(memberName)	                    Ascend a gang member.
-// getInstallResult(memberName)	                Get the effect of an install on ascension multipliers without installing.
-
-// getTaskNames()	                            List member task names.
-// getTaskStats(name)	                        Get stats of a task.
-// setMemberTask(memberName, taskName)          Set gang member to task.
-
-// getEquipmentNames()	                        List equipment names.
-// getEquipmentStats(equipName)	                Get stats of an equipment.
-// getEquipmentType(equipName)	                Get type of an equipment.
-// getEquipmentCost(equipName)	                Get cost of equipment.
-// purchaseEquipment(memberName, equipName)	    Purchase an equipment for a gang member.
-
-// getOtherGangInformation()	                Get information about the other gangs.
-// setTerritoryWarfare(engage)	                Enable/Disable territory clashes.
-// getChanceToWinClash(gangName)	            Get chance to win clash with other gang.
-
-
-// Certain Factions (Slum Snakes, Tetrads, The Syndicate, The Dark Army, Speakers for the Dead, NiteSec, The Black Hand)
-// give the player the ability to form and manage their own gang, which can earn the player money and reputation with the corresponding Faction.
-// Gangs offer more Augmentations than Factions, and in BitNode-2 offer a way to destroy the BitNode.
